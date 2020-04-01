4c496f33ad9ae88b031ed994728ed872a3387b82
zushicat <karinhoehne@akasushi.de>
2020-04-01 19:07:26 +0200

    parse changed DOM

--- get_latest_case_data/_scrape_stadt_koeln.py
+++ get_latest_case_data/_scrape_stadt_koeln.py
@@ -1,8 +1,6 @@
 '''
 Scrape the latest data from the official website of Cologne, Germany
 Url: https://www.stadt-koeln.de/leben-in-koeln/gesundheit/infektionsschutz/corona-virus/corona-virus-anzahl-der-bestaetigten-faelle-koeln
-
-(The data table for the line chart is hidden.)
 '''
 
 import csv
@@ -17,8 +15,6 @@ from bs4 import BeautifulSoup
 import requests
 
 
-locale.setlocale(locale.LC_ALL, '')  # thx to not following date format standards in source: use german local settings
-
 DIRNAME = os.path.dirname(__file__)
 
 def scrape_stadt_koeln_data():
@@ -26,31 +22,36 @@ def scrape_stadt_koeln_data():
     r = requests.get(url)
 
     soup = BeautifulSoup(r.text, 'html.parser')
-    table_container = soup.find(id="datatable_0_2")
+    table_container = soup.find(id="ziel_text_0_21").find("table")
 
     formatted_col_names = {
-        "": "reportDate",
-        "bestätigte Fälle": "cases",
-        "häusliche Quarantäne": "quaratined",
-        "stationäre Behandlung": "hospitalized",
-        "Als geheilt entlassen": "recovered"
+        "Datum": "reportDate",
+        "bestätigte Kölner Fälle": "cases",
+        "Kölner in häuslicher Quarantäne": "quaratined",
+        "Patienten in stationärer Behandlung": "hospitalized",
+        "Kölner als geheilt entlassen": "recovered",
+        "Kölner Todesfälle": "deaths"
     }
 
     column_names: List[str] = []
-    for x in table_container.find("thead").find("tr").contents:
-        col_name: str = formatted_col_names[x.text]
-        column_names.append(col_name)
-
     cologne_data: List[Dict[str, Any]] = []
+    
+    first_row = True
     for row in table_container.find("tbody").contents:
+        if first_row is True:
+            for i, col in enumerate(row):
+                col_name: str = formatted_col_names[col.text]
+                column_names.append(col_name)
+            first_row = False
+            continue
         tmp = {}
         for i, col in enumerate(row):
             if column_names[i] == "reportDate":
-                formatted_date = f"{col.text}"
-                formatted_date = datetime.strptime(formatted_date, "%d. %B").strftime("%d-%m-2020")  # 1. März -> 01-03-2020
+                formatted_date = col.text
+                formatted_date = datetime.strptime(formatted_date, "%d.%m.").strftime("%d-%m-2020")  # 1. März -> 01-03-2020
                 tmp[column_names[i]]:str = formatted_date
             else:
-                tmp[column_names[i]]:str = col.text
+                tmp[column_names[i]]:int = int(col.text.strip().replace(".", ""))
         
         # add static population data (city, not metro)
         tmp["population_number"] = 1085664  
@@ -60,7 +61,6 @@ def scrape_stadt_koeln_data():
 
         cologne_data.append(tmp)
     
-
     today = get_date()
     field_names: List[str] = cologne_data[0].keys()
     with open(f"data/{today}/city_cologne_germany.csv", "w") as f:
