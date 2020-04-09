d25c5bb593c6921c0db18f721d6aa690be7762d1
zushicat <karinhoehne@akasushi.de>
2020-04-09 11:47:43 +0200

    add Tagesspiegel / risklayer request

--- request_data.py
+++ request_data.py
@@ -2,6 +2,7 @@ from get_latest_case_data._request_german_news_outlets import request_news_outle
 from get_latest_case_data._request_german_rki_api import request_rki_api
 from get_latest_case_data._scrape_stadt_koeln import scrape_stadt_koeln_data
 from get_latest_case_data._get_ZON_data import get_ZON_communities_data, get_ZON_international_data
+from get_latest_case_data._get_tagesspiegel_data import get_TS_communities_data
 
 import click
 
@@ -12,6 +13,7 @@ import click
 @click.option('--news', 'requested_source', default=False, flag_value='news', help='Request news outlet API by jgehrcke')
 @click.option('--zon-i', 'requested_source', default=False, flag_value='zon_i', help='Request ZON international numbers')
 @click.option('--zon-c', 'requested_source', default=False, flag_value='zon_c', help='Request ZON community numbers')
+@click.option('--ts', 'requested_source', default=False, flag_value='ts', help='Request Tagesspiegel community numbers')
 @click.option('--cologne', 'requested_source', default=False, flag_value='cologne', help='Request data for city of Cologne, Germany')
 def request_sources(requested_source):
     '''
@@ -22,7 +24,8 @@ def request_sources(requested_source):
         "rki": request_rki_api,
         "cologne": scrape_stadt_koeln_data,
         "zon_i": get_ZON_international_data,
-        "zon_c": get_ZON_communities_data
+        "zon_c": get_ZON_communities_data,
+        "ts": get_TS_communities_data
     }
 
     if requested_source == "all":
