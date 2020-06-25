5db164b7d1009f8eca5d69c8f0ecb69ae13b3e3d
zushicat <karinhoehne@akasushi.de>
2020-06-25 14:22:36 +0200

    update data

--- get_latest_case_data/_scrape_stadt_koeln.py
+++ get_latest_case_data/_scrape_stadt_koeln.py
@@ -49,7 +49,7 @@ def scrape_stadt_koeln_data():
         for i, col in enumerate(row):
             if column_names[i] == "reportDate":
                 formatted_date = col.text
-                formatted_date = formatted_date.replace(".", "")  # try to catch typos in date
+                formatted_date = formatted_date.replace(".", "").strip()  # try to catch typos in date
                 formatted_date = datetime.strptime(formatted_date, "%d%m").strftime("%d-%m-2020")  # 1. März -> 01-03-2020
                 tmp[column_names[i]]:str = formatted_date
             else:
